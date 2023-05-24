baa8397bccdfe1c5527a002c1a1d85372ce148f7
Hutik <mariusz10081997@gmail.com>
2023-05-24 12:00:33 +0200

    Remove 1 folder form main package

--- src/main/java/pl/kowalewski/library/LibraryApplication.java
+++ src/main/java/pl/kowalewski/library/LibraryApplication.java
@@ -1,4 +1,4 @@
-package pl.kowalewski.library.library;
+package pl.kowalewski.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
